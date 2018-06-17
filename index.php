f87d23ca0c7521f39327179ddca72df69b1f738b
Towsif Ahmed Omi <towsif031@gmail.com>
2018-06-17 20:11:24 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Search Engine | Home</title>
+</head>
+
+<body>
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+        </div>
+        <div class="searchContainer">
+            <form action="search.php" method="GET">
+                <input type="text" class="searchBox" name="term">
+                <input type="submit" class="searchButton" value="Search">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
