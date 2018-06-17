b1144c91650a00a27811f378d396da3fd208a090
Towsif Ahmed Omi <towsif031@gmail.com>
2018-06-17 23:27:32 +0600

    added header content in search result page and styled

--- search.php
+++ search.php
@@ -1,3 +1,37 @@
-<?php
-    echo $_GET["term"]
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Search Results - Search Engine</title>
+    <!-- css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="headerContent">
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="assets/images/search_logo.svg" alt="">
+                    </a>
+                </div>
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <div class="searchBarContainer">
+                            <input type="text" class="searchBox" name="term">
+                            <button class="searchButton">
+                                <img src="assets/images/icons/searching_icon.svg" alt="">
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
