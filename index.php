0930bc1c0727010c7a7a2ad55aae0ce002e6c7af
Towsif Ahmed Omi <towsif031@gmail.com>
2018-06-17 20:23:12 +0600

    added basic styles to index page

--- index.php
+++ index.php
@@ -6,18 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Search Engine | Home</title>
+    <!-- css -->
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
-    <div class="mainSection">
-        <div class="logoContainer">
-            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
-        </div>
-        <div class="searchContainer">
-            <form action="search.php" method="GET">
-                <input type="text" class="searchBox" name="term">
-                <input type="submit" class="searchButton" value="Search">
-            </form>
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+            </div>
+            <div class="searchContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBox" name="term">
+                    <input type="submit" class="searchButton" value="Search">
+                </form>
+            </div>
         </div>
     </div>
 </body>
