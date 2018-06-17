1610f8f03a099c10db5c9796911ffa4ec355bdb0
Towsif Ahmed Omi <towsif031@gmail.com>
2018-06-18 00:57:10 +0600

    crawl a site and store the dom document

---
+++ classes/DomDocumentParser.php
@@ -0,0 +1,17 @@
+<?php
+    class DomDocumentParser {
+
+        private $doc;
+
+        public function __construct($url) {
+            $options = array(
+                'http'=>array('method'=>"GET", 'header'=>"User-Agent: searchBot/0.1\n")
+            );
+
+            $context = stream_context_create($options);
+
+            $this->doc = new DomDocument();
+            @$this->doc->loadHTML(file_get_contents($url, false, $context));
+        }
+    }
+?>
